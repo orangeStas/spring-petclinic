ac52cfed4af213ee92ade1575837bb9ccac4a8aa
s.stasiukevich <s.stasiukevich@dearhealth.com>
2020-06-15 20:45:01 +0300

    Clean project setup

--- src/main/java/org/springframework/samples/petclinic/owner/PetType.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetType.java
@@ -15,16 +15,11 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 import org.springframework.samples.petclinic.model.NamedEntity;
 
 /**
  * @author Juergen Hoeller Can be Cat, Dog, Hamster...
  */
-@Entity
-@Table(name = "types")
 public class PetType extends NamedEntity {
 
 }
--- src/main/java/org/springframework/samples/petclinic/visit/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -15,15 +15,9 @@
  */
 package org.springframework.samples.petclinic.visit;
 
-import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.model.BaseEntity;
+
+import java.time.LocalDate;
 
 /**
  * Simple JavaBean domain object representing a visit.
@@ -31,19 +25,12 @@ import org.springframework.samples.petclinic.model.BaseEntity;
  * @author Ken Krebs
  * @author Dave Syer
  */
-@Entity
-@Table(name = "visits")
 public class Visit extends BaseEntity {
 
-	@Column(name = "visit_date")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate date;
 
-	@NotEmpty
-	@Column(name = "description")
 	private String description;
 
-	@Column(name = "pet_id")
 	private Integer petId;
 
 	/**
@@ -51,30 +38,6 @@ public class Visit extends BaseEntity {
 	 */
 	public Visit() {
 		this.date = LocalDate.now();
-	}
-
-	public LocalDate getDate() {
-		return this.date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
-
-	public String getDescription() {
-		return this.description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getPetId() {
-		return this.petId;
-	}
-
-	public void setPetId(Integer petId) {
-		this.petId = petId;
 	}
 
 }
